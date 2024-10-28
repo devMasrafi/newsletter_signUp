17f5e2018825cfc4e20e89f86530a8c209be05ac
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-10-28 11:23:00 +0600

    desktop complete error fixed

--- src/pages/NewsLeter.jsx
+++ src/pages/NewsLeter.jsx
@@ -1,59 +1,86 @@
-import React, { useState } from 'react'
-import SubmitEmail from './SubmitEmail'
+import React, { useState } from "react";
+import SubmitEmail from "./SubmitEmail";
 
 const NewsLeter = () => {
+  const [email, setEmail] = useState();
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-    const [email, setEmail] = useState()
-    const [isSubscribed, setIsSubscribed] = useState(false)
-
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-
-        setIsSubscribed(true)
-        setEmail('')
-    }
+  const handleSubmit = (e) => {
+    setIsSubscribed(true);
+    e.preventDefault();
+    console.log("before", isSubscribed);
+    setEmail("");
+    console.log("after", isSubscribed);
+  };
 
   return (
-    <div className='bg-mainColor'>
-        <div className='container mx-auto py-[120px] h-screen font-roboto '>
-            <div className='flex justify-center items-center bg-slate-100 w-[850px] mx-auto rounded-2xl '>
-                <div className='px-4 '>
-                        <h1 className='font-roboto font-bold text-[54px]  ' >Stay updated!</h1>
-                        <p>Join 60,000+ product managers reciving monthly updates on:</p>
-                        <ul className=' pt-3 pb-3 '>
-                            <li className='flex justify-start items-center ' >
-                                <img className='pr-2 ' src="src/assets/images/icon-list.svg" alt="" />
-                                Product discorvery and building what matters</li>
-                            <li className='flex justify-start items-center '>
-                                <img className='pr-2 ' src="src/assets/images/icon-list.svg" alt="" />
-                                measuring to ensure updates are a success</li>
-                            <li className='flex justify-start items-center '>
-                                <img className='pr-2 ' src="src/assets/images/icon-list.svg" alt="" />
-                                And much more!</li>
-                        </ul>
-                    { isSubscribed ? (
-                        <SubmitEmail isSubscribed={isSubscribed} setIsSubscribed={setIsSubscribed} />
-                    ):(
-                        <form onSubmit={handleSubmit}
-                        className='flex flex-col gap-2' >
-                            <input className='p-1 rounded-md border-none ' 
-                            type="email"
-                            placeholder='example@example.com' name='email'
-                            id='email'
-                            onChange={(e)=>setEmail(e.target.value)} 
-                            />
-                            <button className='bg-mainColor text-white rounded ' type='submit' >submit</button>
-                        </form>
-                    )}
-
-                </div>
-                <div>
-                    <img src="src/assets/images/illustration-sign-up-desktop.svg" alt="sign-up image" />
-                </div>
+    <div className="bg-mainColor">
+      <div className="py-[120px] h-screen font-roboto ">
+        {isSubscribed ? (
+          <SubmitEmail
+            isSubscribed={isSubscribed}
+            setIsSubscribed={setIsSubscribed}
+          />
+        ) : (
+          <div className="flex justify-center items-center bg-slate-100 w-[850px] mx-auto rounded-2xl ">
+            <div className="px-4 ">
+              <h1 className="font-roboto font-bold text-[54px]  ">
+                Stay updated!
+              </h1>
+              <p>Join 60,000+ product managers reciving monthly updates on:</p>
+              <ul className=" pt-3 pb-3 ">
+                <li className="flex justify-start items-center ">
+                  <img
+                    className="pr-2 "
+                    src="src/assets/images/icon-list.svg"
+                    alt=""
+                  />
+                  Product discorvery and building what matters
+                </li>
+                <li className="flex justify-start items-center ">
+                  <img
+                    className="pr-2 "
+                    src="src/assets/images/icon-list.svg"
+                    alt=""
+                  />
+                  measuring to ensure updates are a success
+                </li>
+                <li className="flex justify-start items-center ">
+                  <img
+                    className="pr-2 "
+                    src="src/assets/images/icon-list.svg"
+                    alt=""
+                  />
+                  And much more!
+                </li>
+              </ul>
+              <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                <input
+                  className="p-1 rounded-md border-none "
+                  type="email"
+                  placeholder="example@example.com"
+                  name="email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <button
+                  className="bg-mainColor text-white hover:bg-orange-500 ease-in-out rounded px-2 py-1 "
+                  type="submit">
+                  submit
+                </button>
+              </form>
             </div>
-        </div>
-    </div >
-  )
-}
+            <div>
+              <img
+                src="src/assets/images/illustration-sign-up-desktop.svg"
+                alt="sign-up image"
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default NewsLeter
+export default NewsLeter;
--- src/pages/SubmitEmail.jsx
+++ src/pages/SubmitEmail.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const SubmitEmail = () => {
+const SubmitEmail = ({ isSubscribed, setIsSubscribed }) => {
   return (
-    <div>
-        <div>
-            <img src="src/assets/images/icon-success.svg" alt="check mark" />
-            <h1>
-                Thanks for subscribing!
-            </h1>
-            <p>
-                A confirmation email has been sent. please open it and click the button inside to confirm your subscription.
-            </p>
+    <div className="flex mx-auto justify-center items-center ">
+        <div className="bg-white h-[300px] w-[400px]  p-4 text-center rounded-2xl ">
+            <div className=" pb-12 " >
+                <img className="mx-auto pb-3" src="src/assets/images/icon-success.svg" alt="check mark" />
+                <h1>Thanks for subscribing!</h1>
+                <p>
+                A confirmation email has been sent. please open it and click the
+                button inside to confirm your subscription.
+                </p>
+            </div>
+            <button
+                className=" bg-mainColor text-white hover:bg-orange-500 ease-in-out font-roboto font-semibold p-4 rounded-xl border-none"
+                onClick={(e) => setIsSubscribed(false)}>
+                Dismiss message
+            </button>
         </div>
-        <button onClick={(e) => props.setIsSubscribed(false)}>
-            Dismiss message
-        </button>
-    </div>
-  )
-}
 
-export default SubmitEmail
+    </div>
+  );
+};
+
+export default SubmitEmail;
